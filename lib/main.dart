8a374ad5ed47f5162e0bff410c19cc82140fd834
Zohaib Mughal <z.mughal.24648@khi.iba.edu.pk>
2024-02-22 16:47:54 +0500

    Added Login and Register Button
    Added Login and Register Button , Also Added Password Hide Icon , Added Border on Click

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -7,7 +8,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,11 +22,25 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _obscureText = true;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -33,7 +48,7 @@ class HomePage extends StatelessWidget {
           child: Text(
             'Faculty Reviews',
             style: TextStyle(
-              color: Colors.black,
+              color: Color(0xff700f1a),
               fontSize: 25,
               fontWeight: FontWeight.bold,
               letterSpacing: 2,
@@ -42,52 +57,144 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Expanded(child: Image.network('https://hotsel-ms.vercel.app/assets/logos/ibalogo.png',)),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: IntrinsicHeight(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Image.network(
+                          'https://hotsel-ms.vercel.app/assets/logos/ibalogo.png',
+                          width: deviceWidth > 600 ? 600 : deviceWidth * 0.8,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(20, 1, 20, 10),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            shape: BoxShape.rectangle,
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 1,
+                            ),
+                          ),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: "Erp or Email",
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Color(0xff700f1a), width: 5,),
+                                borderRadius: BorderRadius.all(Radius.circular(10)),
+                              ),
+                            ),
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: GoogleFonts.spaceMono().fontFamily,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsetsDirectional.fromSTEB(20, 1, 20, 10),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            shape: BoxShape.rectangle,
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 1,
+                            ),
+                          ),
+                          child: TextFormField(
+                            obscureText: _obscureText,
+                            decoration: InputDecoration(
+                              hintText: "Enter your Password",
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Color(0xff700f1a), width: 5,),
+                                borderRadius: BorderRadius.all(Radius.circular(10)),
+                              ),
+                              suffixIcon: IconButton(
+                                icon: Icon(_obscureText ? Icons.visibility_off : Icons.visibility),
+                                onPressed: _togglePasswordVisibility,
+                              ),
+                            ),
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: GoogleFonts.spaceMono().fontFamily,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      Row(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsetsDirectional.fromSTEB(20, 1, 20, 10),
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: const Color(0xff700f1a),
+                                    padding: const EdgeInsetsDirectional.fromSTEB(20, 10, 20, 10),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                  child: const Text(
+                                    'Login',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsetsDirectional.fromSTEB(20, 1, 20, 10),
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor : const Color(0xff700f1a),
+                                    padding: const EdgeInsetsDirectional.fromSTEB(20, 10, 20, 10),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                  child: const Text(
+                                    'Register',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
 
-          Padding(
-            padding: const EdgeInsetsDirectional.fromSTEB(20,1,20,10),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 1,
+                          ],
+                      )
+                    ],
+                  ),
                 ),
               ),
-              child: TextFormField(decoration: const InputDecoration(hintText: "Email or ERP" ), style :TextStyle(
-                color: Colors.white,
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1.5,
-                fontFamily: GoogleFonts.spaceMono().fontFamily,
-              ),
-
-                  textAlign: TextAlign.center),),
-
-          ),
-          Padding(
-            padding: const EdgeInsetsDirectional.fromSTEB(20,1,20,10),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 1,
-                ),
-              ),
-              child: TextFormField(decoration: const InputDecoration(hintText: "Enter your Password" ), style :TextStyle(
-                color: Colors.white,
-                fontSize: 15,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1.5,
-                fontFamily: GoogleFonts.spaceMono().fontFamily,
-              ),
-
-                  textAlign: TextAlign.center),),
-
-          ),
-        ],
+            ),
+          );
+        },
       ),
       backgroundColor: const Color(0xffffffff),
     );
